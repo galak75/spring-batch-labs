4d4b6fe7ba25852f04211c1a293b55f8105669a8
Géraud <galak75@gmail.com>
2014-04-30 18:30:39 -0400

    Add test on parallel flows

---
+++ src/main/java/org/galak75/split/ParallelFlowsJobConfig.java
@@ -0,0 +1,108 @@
+package org.galak75.split;
+
+import org.galak75.TraceListener;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.job.builder.FlowBuilder;
+import org.springframework.batch.core.job.flow.support.SimpleFlow;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+
+@Configuration
+public class ParallelFlowsJobConfig {
+
+    @Autowired
+    private JobBuilderFactory jobs;
+    @Autowired
+    private StepBuilderFactory steps;
+
+    @Bean
+    public Job job() {
+        return jobs.get("Job with parallel flows")
+                .start(mainFlow())
+                .build()
+                .listener(traceListener())
+                .build();
+    }
+
+    @Bean
+    public SimpleFlow mainFlow() {
+        return new FlowBuilder<SimpleFlow>("Main Flow")
+                .start(flow1())
+                .split(new SimpleAsyncTaskExecutor())
+                .add(flow2(), flow3())
+                .build();
+    }
+
+    @Bean
+    public SimpleFlow flow1() {
+        return new FlowBuilder<SimpleFlow>("flow 1")
+                .start(step1())
+                .build();
+    }
+
+    @Bean
+    public SimpleFlow flow2() {
+        return new FlowBuilder<SimpleFlow>("flow 2")
+                .start(step2())
+                .build();
+    }
+
+    @Bean
+    public SimpleFlow flow3() {
+        return new FlowBuilder<SimpleFlow>("flow 3")
+                .start(step3())
+                .build();
+    }
+
+    @Bean
+    public Step step1() {
+        return steps.get("Step 1")
+                .tasklet(waitingTasklet())
+                .listener(traceListener())
+                .build();
+    }
+
+    @Bean
+    public Step step2() {
+        return steps.get("Step 1")
+                .tasklet(waitingTasklet())
+                .listener(traceListener())
+                .build();
+    }
+
+    @Bean
+    public Step step3() {
+        return steps.get("Step 1")
+                .tasklet(waitingTasklet())
+                .listener(traceListener())
+                .build();
+    }
+
+    @Bean
+    public Tasklet waitingTasklet() {
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                System.out.println(String.format("  * Starting step '%s'", chunkContext.getStepContext().getStepName()));
+                Thread.sleep(1000);
+                System.out.println(String.format("  * Stopping step '%s'", chunkContext.getStepContext().getStepName()));
+                return RepeatStatus.FINISHED;
+            }
+        };
+    }
+
+    @Bean
+    public TraceListener traceListener() {
+        return new TraceListener();
+    }
+
+}
---
+++ src/test/java/org/galak75/split/ParallelFlowsJobConfigTest.java
@@ -0,0 +1,28 @@
+package org.galak75.split;
+
+import org.galak75.SpringBatchInfrastructureITestConfiguration;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.batch.core.ExitStatus;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import static org.junit.Assert.assertEquals;
+
+@ContextConfiguration(classes = {SpringBatchInfrastructureITestConfiguration.class, ParallelFlowsJobConfig.class})
+@RunWith(SpringJUnit4ClassRunner.class)
+public class ParallelFlowsJobConfigTest {
+
+    @Autowired
+    private JobLauncherTestUtils jobLauncherTestUtils;
+
+    @Test
+    public void test() throws Exception {
+        JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+
+        assertEquals(ExitStatus.COMPLETED, jobExecution.getExitStatus());
+    }
+}
