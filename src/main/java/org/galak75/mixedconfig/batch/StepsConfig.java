901abe097e1c3929a75f85a77f8f038411f7c5c4
Géraud <galak75@gmail.com>
2015-10-18 18:06:54 -0400

    Defining a stand-alone tasklet

--- src/main/java/org/galak75/mixedconfig/batch/StepsConfig.java
+++ src/main/java/org/galak75/mixedconfig/batch/StepsConfig.java
@@ -1,8 +1,11 @@
 package org.galak75.mixedconfig.batch;
 
-import org.galak75.SystemPropertyTasklet;
 import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -28,8 +31,14 @@ public class StepsConfig {
     }
 
     @Bean
-    public SystemPropertyTasklet commonTasklet() {
-        return new SystemPropertyTasklet("COMMON_EXIT");
+    public Tasklet commonTasklet() {
+        return new Tasklet() {
+            @Override
+            public RepeatStatus execute(StepContribution stepContribution, ChunkContext chunkContext) throws Exception {
+                System.out.println(String.format("Executing %s ...", chunkContext.getStepContext().getStepName()));
+                return null;
+            }
+        };
     }
 
 }
